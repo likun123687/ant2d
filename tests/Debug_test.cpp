96383a349231e6965e8727c553c678235e725eb7
likun123687 <378186878@qq.com>
2022-05-26 20:54:08 +0800

    delete some test file

--- tests/Debug_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <utils/Debug.h>
-int main()
-{
-    Info("i ame a info message {}--{}", "message1", "message2");
-    Error("i ame a Error message");
-    Warn("i ame a warn message");
-    return 0;
-}
