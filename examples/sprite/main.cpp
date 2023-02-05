e89f8fd91acbea8833263f65fd9b28e38149ae37
likun123687 <378186878@qq.com>
2023-02-06 00:33:04 +0800

    fix

--- examples/sprite/main.cpp
+++ examples/sprite/main.cpp
@@ -14,14 +14,23 @@ class MainScene : public ant2d::Scene {
     void OnEnter(ant2d::Game* g)
     {
         Info("main scene on enter");
-        auto tex = ant2d::SharedTextureManager->Get("assets/face.png");
+        auto tex = ant2d::SharedTextureManager->Get("assets/block.png");
         auto repo = ant2d::SharedTextureManager->GetRepo();
-        assert(repo["assets/face.png"].rid == tex->GetTextureId());
+        // assert(repo["assets/face.png"].rid == tex->GetTextureId());
 
         auto entity = ant2d::SharedEntityManager->New();
         ant2d::SharedSpriteTable->NewCompX(entity, tex)->SetSize(100, 100);
         auto xf = ant2d::SharedTransformTable->NewComp(entity);
-        xf->SetPosition(ant2d::math::Vec2 { 100, 100 });
+        xf->SetPosition(ant2d::math::Vec2 { 50, 50 });
+
+        for (int i = 0; i < 16000; i++) {
+            auto entity = ant2d::SharedEntityManager->New();
+            ant2d::SharedSpriteTable->NewCompX(entity, tex)->SetSize(100, 100);
+            auto xf = ant2d::SharedTransformTable->NewComp(entity);
+            float x = ant2d::math::Random(0, 480);
+            float y = ant2d::math::Random(0, 200) + 120;
+            xf->SetPosition(ant2d::math::Vec2 { x, y });
+        }
 
         // auto entity1 = ant2d::SharedEntityManager->New();
         // ant2d::SharedSpriteTable->NewCompX(entity1, tex)->SetSize(80, 80);
@@ -79,6 +88,7 @@ ant2d::WindowOptions* ant2d_main(int argc, char* argv[])
         ant2d::SharedTextureManager->Load("assets/face.png");
         ant2d::SharedTextureManager->Load("assets/face1.png");
         ant2d::SharedTextureManager->Load("assets/1111.png");
+        ant2d::SharedTextureManager->Load("assets/block.png");
         ant2d::SharedTextureManager->Load("assets/debug_font.png");
 
         ant2d::SharedTextureManager->LoadAtlas("assets/test.png", "assets/test.json");
